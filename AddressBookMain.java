a434bfaf0c6645dd80e49b5652a1e15df29c737f
SourabhIngle <100rabhingle@gmail.com>
2023-03-12 12:23:44 +0530

    UC4

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,6 +6,8 @@ public class AddressBookMain extends AddressBook {
     public static void main(String[] args) {
         AddressBookMain personInfo = new AddressBookMain();
         personInfo.addContact();
+        personInfo.editContact();
+        personInfo.deleteContact();
         System.out.println(personInfo.people);
     }
 }
